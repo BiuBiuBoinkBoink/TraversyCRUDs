c5dcc3f28b84cd7dff839b49a964b460dd31d7f2
BiuBiuBoinkBoink <123261321+BiuBiuBoinkBoink@users.noreply.github.com>
2023-08-07 19:15:13 +0800

    ola

--- routes/auth.js
+++ routes/auth.js
@@ -9,10 +9,9 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc Google auth callback 
 // @route GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/'}), 
-    (req, res) => { 
-        res.redirect('/dashboard')
-    }
-)
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req,res) => {
+    res.redirect('/dashboard')
+})
+
 
 module.exports = router 
