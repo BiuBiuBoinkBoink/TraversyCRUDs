db7be3a33fd905063d9ff67aed7a99e8af38e8ca
BiuBiuBoinkBoink <123261321+BiuBiuBoinkBoink@users.noreply.github.com>
2023-08-09 23:43:26 +0800

    ola

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./config/db')
 
 //Load config 
@@ -39,6 +40,9 @@ app.use(session({
     secret: 'keyboard cat', 
     resave: false, 
     saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
 }))
 
 // Passport middleware 
@@ -53,7 +57,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes 
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 5000
 
